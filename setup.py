ec6063aa7c4fec798f1f6e98962419d6a5c20ebd
qodzero <fourkay537@gmail.com>
2020-03-11 14:30:43 +0200

    updated setup.py and added conf file

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
 	name='icsv',
-	version='0.1.4',
+	version='0.1.6',
 	scripts=['icsv/csv'],
+	license='MIT',
+	keywords=['csv'],
 	author='Samuel Mtembo',
 	author_email='samuel@samuelworks.org',
 	description='A simple script to make operating csv files easier',
 	long_description=long_description,
-	url='https://github.com/qodzero/icsv',
+	url='https://www.samuelworks.org',
+	download_url='https://github.com/qodzero/icsv/archive/v1.0.5.tar.gz',
 	packages=setuptools.find_packages(),
 	classifiers=[
 			"Programming Language ::Python :: 3",
